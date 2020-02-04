aa8a8dcca12299ee9c4cf0b872c7098049365544
crais01 <ccortesj.01@gmail.com>
2020-02-04 00:06:35 -0300

    l

--- controlador/c_traerMunicipio.php
+++ controlador/c_traerMunicipio.php
@@ -3,12 +3,12 @@
 
 $resMunicipio = traerMunicipio();
 
-if(is_null($resMunicipio)){
+if (is_null($resMunicipio)) {
     echo "<option>no existen municipios</option>";
-}else{
-    foreach($resMunicipio as $row)
-    {
-        echo "<option value='".$row['codigo_municipalidad']."'>".$row['nombre_municipalidad']."</option>";
+} else {
+    foreach ($resMunicipio as $row) {
+        echo "<option value='" . $row['codigo_municipalidad'] . "'>" . $row['nombre_municipalidad'] .
+            "</option>";
     }
 
 }
